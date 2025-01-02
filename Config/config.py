afb3c436824c122a7b3f41b105d9b14e1b564ad2
Kaliboy002 <146410941+Kaliboy002@users.noreply.github.com>
2025-01-02 18:24:30 +0430

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -1,8 +1,8 @@
 # Telegram bot config
-token = 'BOT TOKEN'
-api_hash = "API HASH"
-api_id = 1234  # API ID
-admin_id = 1234  # ADMIN ID
+token = '8179647576:AAGPGp13Pp-qF32KqDivMTEn-4LHOLcpRpI'
+api_hash = "e51a3154d2e0c45e5ed70251d68382de"
+api_id = 15787995  # API ID
+admin_id = 7046488481  # ADMIN ID
 
 # Mysql Database config
 host = "localhost"
